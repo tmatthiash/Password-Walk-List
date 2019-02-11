59ba9a20150bbc5f5ccb8e453e9e4e46581f686e
root <tmatthiash@hotmail.com>
2019-02-11 17:35:44 -0600

    Optimized a litte and added license

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const BlacklistedPasswords = require('./PasswordWalks.json')
 
-export default function passwordMatchFound(password) {
-    const matches = BlacklistedPasswords.reduce((accumulator=0, currentValue) => {
-        if(password.includes(currentValue)){
-            return accumulator + 1
-        }
-        return accumulator
-    },0)
-    if(matches>0){
+export default Array.of(BlacklistedPasswords).forEach((pattern)=>{
+    if(password.includes(pattern)){
         return true
     }
-    return false
-}
-
+})
+return false
