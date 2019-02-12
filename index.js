abca0a46771770404ed659ff2c2f4c3ed94b3469
root <tmatthiash@hotmail.com>
2019-02-11 18:12:17 -0600

    Undid mistakes

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+
 const BlacklistedPasswords = require('./PasswordWalks.json')
 
-export default Array.of(BlacklistedPasswords).forEach((pattern)=>{
-    if(password.includes(pattern)){
+export default function passwordMatchFound(password) {
+    const matches = BlacklistedPasswords.reduce((accumulator=0, currentValue) => {
+        if(password.includes(currentValue)){
+            return accumulator + 1
+        }
+        return accumulator
+    },0)
+    if(matches>0){
         return true
     }
     return false
-})
-
+}
